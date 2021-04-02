d970b6339e534ac63db1f4a8ad0ea07663677dd2
KALININ ALEXEY <2257855@gmail.com>
2021-04-02 18:48:12 +0300

    1

--- js/main.js
+++ js/main.js
@@ -18,7 +18,29 @@ const swiper = new Swiper(".swiper-container", {
   },
 });
 
+var modalButton = document.querySelector(".header-top__button");
 var menuButton = document.querySelector(".header-top__menu-button");
-menuButton.addEventListener('click', function() {
-  console.log(11111111111111);
-})
+var landings = document.querySelector(".header-top__menu-link--landings");
+var pages = document.querySelector(".header-top__menu-link--pages");
+
+modalButton.addEventListener("click", function () {
+  console.log("moadl");
+});
+
+menuButton.addEventListener("click", function () {
+  console.log("menu");
+});
+
+landings.addEventListener("click", function () {
+  console.log("landings");
+  document
+    .querySelector(".header-top__landings")
+    .classList.toggle("header-top__landings--visible");
+});
+
+pages.addEventListener("click", function () {
+  console.log("pages");
+  document
+    .querySelector(".header-top__pages")
+    .classList.toggle("header-top__pages--visible");
+});
