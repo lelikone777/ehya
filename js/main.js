bee387ce634106760ae11ea6602db1c6670c5e15
KALININ ALEXEY <2257855@gmail.com>
2021-04-04 14:27:53 +0300

    modal dialog

--- js/main.js
+++ js/main.js
@@ -38,10 +38,12 @@ $(document).ready(function () {
     },
   });
 
-  var modalButton = document.querySelector(".header-top__button");
+  // var modalButton = document.querySelector(".header-top__button");
   var menuButton = document.querySelector(".header-top__menu-button");
   var landings = document.querySelector(".header-top__menu-link--landings");
   var pages = document.querySelector(".header-top__menu-link--pages");
+  var landingsMenu = document.querySelector(".header-top__landings");
+  var pagesMenu = document.querySelector(".header-top__pages");
 
   menuButton.addEventListener("click", function () {
     console.log("menu");
@@ -55,14 +57,8 @@ $(document).ready(function () {
     document
       .querySelector(".header-top__landings")
       .classList.toggle("header-top__landings--visible");
-    if (
-      document
-        .querySelector(".header-top__pages")
-        .classList.contains("header-top__pages--visible")
-    ) {
-      document
-        .querySelector(".header-top__pages")
-        .classList.remove("header-top__pages--visible");
+    if (pagesMenu.classList.contains("header-top__pages--visible")) {
+      pagesMenu.classList.remove("header-top__pages--visible");
     }
   });
 
@@ -71,26 +67,34 @@ $(document).ready(function () {
     document
       .querySelector(".header-top__pages")
       .classList.toggle("header-top__pages--visible");
-    if (
-      document
-        .querySelector(".header-top__landings")
-        .classList.contains("header-top__landings--visible")
-    ) {
-      document
-        .querySelector(".header-top__landings")
-        .classList.remove("header-top__landings--visible");
+    if (landingsMenu.classList.contains("header-top__landings--visible")) {
+      document;
+      landingsMenu.classList.remove("header-top__landings--visible");
     }
   });
 
-  //   var modalButton = $("[data-toggle=modal]");
-  //   var closeModalButton = $(".modal__close");
-  //   modalButton.on("click", openModal);
-  //   closeModalButton.on("click", closeModal);
 
-  // function openModal() {
-  //   var targetModal = $(this).attr("data-href");
-  //   $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
-  //   $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
-  //   document.body.classList.toggle("lock");
-  // }
+
+  
+  var modalButton = $("[data-toggle=modal]");
+  console.log(modalButton);
+  modalButton.on("click", openModal);
+  var closeModalButton = $(".modal__close");
+  closeModalButton.on("click", closeModal);
+  var modalOverlay = $(".modal__overlay");
+  modalOverlay.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+    document.body.classList.toggle("lock");
+  }
+   function closeModal(event) {
+     event.preventDefault();
+     var modalDialog = $(".modal__dialog");
+     modalOverlay.removeClass("modal__overlay--visible");
+     modalDialog.removeClass("modal__dialog--visible");
+     document.body.classList.remove("lock");
+   }
 });
