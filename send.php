69678a56e24562f8c2cb19f07769f006a5a47f48
KALININ ALEXEY <2257855@gmail.com>
2021-04-05 21:13:36 +0300

    email

--- send.php
+++ send.php
@@ -8,6 +8,7 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 
 // Формирование самого письма
@@ -15,6 +16,7 @@ $title = "Новое обращение Ehya";
 $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
+<b>email:</b> $email<br>
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
 ";
